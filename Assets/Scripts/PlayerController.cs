f08cc9b558da0aecb82ad6fc6c84205eee5e8dee
zion <jaewoongchoi46@gmail.com>
2025-10-02 21:12:05 +0900

    update gitignore & player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float speed = 3;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            move += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            move += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            move += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            move += new Vector3(0, -1, 0);
+        }
+
+        move = move.normalized;
+        transform.Translate(move * speed * Time.deltaTime);
+    }
+}
