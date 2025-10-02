13be5e5ad39ac5b560d9c7aaca4cec02732bad53
zion <jaewoongchoi46@gmail.com>
2025-10-02 23:40:13 +0900

    add player collider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 
+
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float speed = 3;
+    Vector3 move;
 
     void Start()
     {
@@ -11,26 +13,25 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     void Update()
     {
-        Vector3 move = Vector3.zero;
+        move = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
             move += new Vector3(-1, 0, 0);
         }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
             move += new Vector3(1, 0, 0);
         }
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-        {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
             move += new Vector3(0, 1, 0);
         }
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-        {
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
             move += new Vector3(0, -1, 0);
         }
-
         move = move.normalized;
+    }
+
+    private void FixedUpdate()
+    {
         transform.Translate(move * speed * Time.deltaTime);
     }
 }
