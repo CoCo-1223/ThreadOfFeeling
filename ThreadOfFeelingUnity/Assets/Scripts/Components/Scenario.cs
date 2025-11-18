acebfecf2df2bad059d6f281e5a085cbdfca872d
CoCo-1223 <kimco2104@naver.com>
2025-11-18 16:18:45 +0900

    add scenario&story

--- ThreadOfFeelingUnity/Assets/Scripts/Components/Scenario.cs
+++ ThreadOfFeelingUnity/Assets/Scripts/Components/Scenario.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Components
 {
@@ -6,11 +7,12 @@ namespace Components
     public class Scenario {
         [Header("기본 내용")]
         public Sprite image;
+
         [TextArea(3, 10)]
         public string dialogueText;
 
-        [Header("퀴즈 (선택 사항)")]
+        [Header("퀴즈 (3개)")]
         [Tooltip("이 시나리오의 퀴즈 데이터. 없으면 null로 두거나 퀴즈의 Question Text를 비워두세요.")]
-        public Question quiz;
+        public List<Question> quizzes = new List<Question>();
     }
 }
